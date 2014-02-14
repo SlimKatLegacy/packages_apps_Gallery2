d855038363b718f43bbdc1c280eab5e9f4da9a00
wjiang <wjiang@codeaurora.org>
2014-02-14 17:45:53 +0800

    Gallery2: Fix batches of issues in movie effects
    Fix following issues:
    - All settings aren't restored to previous state when user cancel
    his operations.
    - Check the validity to avoid calling member function of an already
    released object.
    - Recycle effect instance when MovieActivity pauses.
    - Don't restore strength from pref when toggling switch.
    - Force the Effect's dialog to be uncancelable.
    - Fix null pointer and effect leak issue due to CMCC features.
    - Fix audio effects dialog display on small screens
    - Recognize bluetooth headset.
    
    Change-Id: I803897508c0c1a6723170b7691e3ece03680e4f1
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/android/gallery3d/app/MoviePlayer.java
+++ src/com/android/gallery3d/app/MoviePlayer.java
@@ -1144,6 +1144,7 @@ public class MoviePlayer implements
                 mVideoView.stopPlayback();
                 mVideoView.setVisibility(View.INVISIBLE);
                 clearVideoInfo();
+                mActivityContext.releaseEffects();
                 mMovieItem = next;
                 mActivityContext.refreshMovieInfo(mMovieItem);
                 doStartVideo(false, 0, 0);
@@ -1180,6 +1181,7 @@ public class MoviePlayer implements
             mVideoView.setVisibility(View.INVISIBLE);
             mVideoView.setVisibility(View.VISIBLE);
             clearVideoInfo();
+            mActivityContext.releaseEffects();
             mFirstBePlayed = false;
             mController.setCanReplay(true);
             mController.showEnded();
